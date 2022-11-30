13e1c1d30267b79c38daf5b9b947c64bc0d64fd4
leopan0529 <jiabinpan.leo@gmail.com>
2022-11-30 16:42:47 -0600

    add tests and ability to say 'hello' to any input.

--- code.js
+++ code.js
@@ -2,3 +2,22 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+
+
+
+function sayHello(input){
+
+    return `Hello,${input}!`
+    // if(input === "Pat"){
+    //     return `Hello,${input}!`
+    // }else if(input === "Alex"){
+    //     return `Hello,${input}!`
+    // }
+
+}
+
+
+sayHello("Alex")
+sayHello("Pat")
+sayHello("Jane")
--- tests.js
+++ tests.js
@@ -13,3 +13,42 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+
+
+
+
+describe('sayHello',function (){
+    it('should be a defined function',function (){
+        expect( typeof sayHello).toBe('function')
+    })
+})
+
+describe('sayHello',function () {
+    it('should return a string when called', function () {
+        expect(typeof sayHello()).toBe("string")
+    });
+})
+
+
+
+describe("sayHello",function (){
+    it(`should return the string Hello,Jane!`,function (){
+        expect(sayHello("Jane")).toBe(`Hello,Jane!`)
+
+    })
+})
+
+describe('sayHello',function (){
+    it('should return the string Hello, Alex',function (){
+        expect(sayHello("Alex")).toBe("Hello,Alex!")
+    })
+})
+
+
+
+describe('sayHello',function (){
+    it('should return the string Hello, Pat!',function (){
+        expect(sayHello("Pat")).toBe("Hello,Pat!")
+    })
+})
